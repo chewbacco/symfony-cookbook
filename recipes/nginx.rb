ac939a7a64df4a7807310c3ec90ed730c68411c5
Dmitry Tsoy <hd.deman@gmail.com>
2013-11-25 23:13:37 +0400

    split nginx and default cookbook

---
+++ recipes/nginx.rb
@@ -0,0 +1,11 @@
+#1.4.3
+include_recipe "nginx"
+
+template "#{node[:nginx][:dir]}/sites-available/#{node[:symfony][:domain]}.conf" do
+  source "nginx-site.conf.erb"
+  notifies :reload, "service[nginx]"
+end
+
+nginx_site "#{node[:symfony][:domain]}.conf" do
+  enable true
+end
