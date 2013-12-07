c04e27b0dcbdbcfc8ef931fb375e1d8ea48316a9
Dmitry Tsoy <hd.deman@gmail.com>
2013-12-08 00:38:50 +0400

    rubocop fixes

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,47 +15,47 @@ require 'spec_helper'
 # end
 
 describe 'symfony::default' do
-    let(:runner) do
-        ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04')  do |node|
-            node.set[:mysql][:server_root_password] = 'rootpass'
-            node.set[:mysql][:server_debian_password] = 'debpass'
-            node.set[:mysql][:server_repl_password] = 'replpass'
-            node.set[:symfony][:database][:password] = 'myapp_1234'
-            node.set[:symfony][:database][:name] = 'symfony'
-            node.set[:symfony][:database][:user] = 'symfony'
-        end.converge(described_recipe)
-    end
+  let(:runner) do
+    ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04')  do |node|
+      node.set[:mysql][:server_root_password] = 'rootpass'
+      node.set[:mysql][:server_debian_password] = 'debpass'
+      node.set[:mysql][:server_repl_password] = 'replpass'
+      node.set[:symfony][:database][:password] = 'myapp_1234'
+      node.set[:symfony][:database][:name] = 'symfony'
+      node.set[:symfony][:database][:user] = 'symfony'
+    end.converge(described_recipe)
+  end
 
-    before do
-        stub_command("test -d /etc/php5/fpm/pools || mkdir -p /etc/php5/fpm/pools").and_return(true)
-        stub_command("which sass").and_return(true)
-    end
+  before do
+    stub_command('test -d /etc/php5/fpm/pools || mkdir -p /etc/php5/fpm/pools').and_return(true)
+    stub_command('which sass').and_return(true)
+  end
 
-    it 'includes the git recipe' do
-        expect(runner).to include_recipe('git::default')
-    end
-    it 'includes the symfony::nginx recipe' do
-        expect(runner).to include_recipe('symfony::nginx')
-    end
-    it 'includes the composer recipe' do
-        expect(runner).to include_recipe('composer::default')
-    end
-    it 'includes the php-fpm recipe' do
-        expect(runner).to include_recipe('php-fpm::default')
-    end
-    it 'includes the php recipe' do
-        expect(runner).to include_recipe('php::default')
-    end
-    it 'includes the php::module_mysql recipe' do
-        expect(runner).to include_recipe('php::module_mysql')
-    end
-    it 'includes the mysql::client recipe' do
-        expect(runner).to include_recipe('mysql::client')
-    end
-    it 'includes the mysql::server recipe' do
-        expect(runner).to include_recipe('mysql::server')
-    end
-    it 'includes the database::mysql recipe' do
-        expect(runner).to include_recipe('database::mysql')
-    end
+  it 'includes the git recipe' do
+    expect(runner).to include_recipe('git::default')
+  end
+  it 'includes the symfony::nginx recipe' do
+    expect(runner).to include_recipe('symfony::nginx')
+  end
+  it 'includes the composer recipe' do
+    expect(runner).to include_recipe('composer::default')
+  end
+  it 'includes the php-fpm recipe' do
+    expect(runner).to include_recipe('php-fpm::default')
+  end
+  it 'includes the php recipe' do
+    expect(runner).to include_recipe('php::default')
+  end
+  it 'includes the php::module_mysql recipe' do
+    expect(runner).to include_recipe('php::module_mysql')
+  end
+  it 'includes the mysql::client recipe' do
+    expect(runner).to include_recipe('mysql::client')
+  end
+  it 'includes the mysql::server recipe' do
+    expect(runner).to include_recipe('mysql::server')
+  end
+  it 'includes the database::mysql recipe' do
+    expect(runner).to include_recipe('database::mysql')
+  end
 end
