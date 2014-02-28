0efc84c378d9095ddf8ac39f1b8abc7e25c5b036
Dmitry Tsoy <hd.deman@gmail.com>
2014-02-28 18:35:48 +0400

    downgrade nginx cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Deploys and configures Symfony-based applications'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.5'
 
-depends 'nginx', '~> 2.4.1'
+depends 'nginx', '~> 2.3.0'
 depends 'php-fpm', '~> 0.6.3'
 depends 'php', '~> 1.4.2'
 depends 'mysql', '~> 4.1.2'
