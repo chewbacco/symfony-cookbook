d7b96e35b6e0e8fbf62c0bff2569a34db57ffc1a
Dmitry Tsoy <hd.deman@gmail.com>
2014-04-29 16:07:14 +0400

    added aliases for nginx conf

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,11 +1,12 @@
 # 1.4.4
 include_recipe 'nginx'
 
-node[:symfony][:projects].each { |project| 
+node[:symfony][:projects].each { |project|
   template "#{node[:nginx][:dir]}/sites-available/#{project[:domain]}.conf" do
     source 'nginx-site.conf.erb'
     variables(
        'domain' => project[:domain],
+       'aliases' => project[:aliases],
        'document_root' => project[:document_root],
        'remove_trailing_slash' => project[:remove_trailing_slash],
        'index_file' => project[:index_file],
