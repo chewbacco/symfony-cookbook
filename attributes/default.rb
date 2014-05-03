b6c48d185f53c2e7941b6813924d768e29e0dd1d
Dmitry Tsoy <hd.deman@gmail.com>
2014-05-03 15:19:08 +0400

    fix defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@ default[:symfony][:user] = 'symfony-app'
 default[:symfony][:group] = 'symfony-app'
 default[:symfony][:remove_trailing_slash] = false
 
-default[:nginx][:default_site_enabled] = false
-default[:nginx][:install_method] = 'source'
-default[:nginx][:version] = '1.4.4'
-default[:nginx][:configure_flags] = ['--with-http_stub_status_module']
+override[:nginx][:default_site_enabled] = false
+override[:nginx][:install_method] = 'source'
+override[:nginx][:version] = '1.4.7'
+override[:nginx][:configure_flags] = ['--with-http_stub_status_module']
 
 default[:php][:packages] = %w(php5 php5-dev php5-cli php-pear)
 
