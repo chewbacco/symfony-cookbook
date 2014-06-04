ec2a0b24c5c7f0684fa6b67488a06f860aa9b3b4
Dmitry Tsoy <hd.deman@gmail.com>
2014-06-05 00:53:47 +0400

    update ruby

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,11 +43,11 @@ include_recipe 'git::default'
 include_recipe 'php'
 # include_recipe 'rvm::system'
 
-include_recipe "rbenv::default"
-include_recipe "rbenv::ruby_build"
+include_recipe 'rbenv::default'
+include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby "2.1.1" do
-  ruby_version "2.1.1"
+rbenv_ruby '2.1.2' do
+  ruby_version '2.1.2'
   global true
 end
 
