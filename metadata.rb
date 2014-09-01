6869af0a33be90d48599e741d8d31a9d6a2b2967
Dmitry Tsoy <hd.deman@gmail.com>
2014-09-01 18:17:52 +0400

    update cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ description      'Deploys and configures Symfony-based applications'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.6'
 
-depends 'nginx', '~> 2.7.0'
-depends 'php-fpm', '~> 0.6.8'
+depends 'nginx', '~> 2.7.4'
+depends 'php-fpm', '~> 0.6.10'
 depends 'php', '~> 1.4.6'
-depends 'mysql', '~> 5.1.8'
-depends 'database', '~> 2.1.6'
-depends 'rbenv', '~> 1.6.5'
+depends 'mysql', '~> 5.4.0' #https://github.com/opscode-cookbooks/database/issues/79
+depends 'database', '~> 2.3.0'
+depends 'rbenv', '~> 1.7.1'
 depends 'git', '~> 4.0.2'
-depends 'composer', '~> 1.0.0'
+depends 'composer', '~> 1.0.4'
